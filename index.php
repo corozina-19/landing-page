d794ac0db7462a43c6e163aa4a8eb767efd62ba5
Luis Moncaris <lmoncarisg@gmail.com>
2020-04-02 01:18:48 -0500

    [Initial] initial wordpress theme without any interaction only html

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ucare</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/styles.css">
+</head>
+<body>
+    <div class="mainHeader">
+        <img src="<?php echo get_template_directory_uri() ?>/img/Logo.head.svg" class="mainLogo">
+        <div class="mainMenu">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about-us.html">Sobre</a></li>
+                <li><a href="/contact.html">Contacto</a></li>
+                <li><a href="/login.html" class="btn-overline">Entrar</a></li>
+                <li><a href="/diagnosis.html" class="btn-success-header">Diagnóstico</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="mainSection">
+        <div class="leftMainSection">
+            <h2 class="tagLine">Cuéntanos que sientes y recibe asistencia médica</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed Lorem ipsum dolor sit amet.</p>
+            <h4>Disponible en:</h4>
+            <div class="storesSection">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.google.play.svg" alt="Gooogle play">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.app.store.svg" alt="App store">
+            </div>
+        </div>
+        <div class="rightMainSection">
+            <div class="doctor"></div>
+            <div class="signal"></div>
+            <div class="hand"></div>
+        </div>
+    </div>
+    <div class="mainInfoSection">
+        <div class="infoSectionGap"></div>
+        <div class="infoSection">
+            <img src="<?php echo get_template_directory_uri() ?>/img/btn.diagnostico.svg" class="infoSectionIcon" alt="">
+            <h5>
+                Diagnóstico
+            </h5>
+            <p>
+                Realiza un diagnóstico del COVID-19 desde tu casa.
+            </p>
+        </div>
+        <div class="infoSection">
+            <img src="./img/btn.asis.medica.svg" class="infoSectionIcon" alt="">
+            <h5>
+                Asistencia médica
+            </h5>
+            <p>
+                Recibe apoyo médico en caso que lo necesites.
+            </p>
+        </div>
+        <div class="infoSection">
+            <img src="<?php echo get_template_directory_uri() ?>/img/btn.informacion.svg" class="infoSectionIcon" alt="">
+            <h5>
+                Información
+            </h5>
+            <p>
+                Informaté con los expertos , con artículos oficiales.
+            </p>
+        </div>
+        <div class="infoSectionGap"></div>
+    </div>
+    <div class="mainDiagnosis">
+        <div class="mainDiagnosisGap"></div>
+
+        <div class="inHomeDiagnosis">
+            <div class="diagnosisBodyimage"></div>
+            <h4>
+                Diagnóstico en casa
+            </h4>
+            <div class="diagnosisList">
+                <div class="diagnosisListItem">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Identifica si tienes una gripa común</p>
+                </div>
+                <div class="diagnosisListItem">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Recibe la asistencia médica en étapas tempranas</p>
+                </div>
+                <div class="diagnosisListItem">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Informáte con artículos verificados sobre el COVID-19</p>
+                </div>
+                <div class="diagnosisListItem">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Comparte Ucare a quienes lo necesitan en sus casas</p>
+                </div>
+                <div class="diagnosisListItem">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Descongestiona los hospitales evitando una crisis</p>
+                </div>
+                <div class="diagnosisListItem"> 
+                    <img src="<?php echo get_template_directory_uri() ?>/img/btn.check.bodyicon.png" alt="" />
+                    <p>Juntos nos cuidamos y le ganamos al COVID-19</p>
+                </div>
+            </div>
+        </div>
+        <div class="socialNetworks">
+            <a class="twitter-timeline" href="https://twitter.com/MinSaludCol?ref_src=twsrc%5Etfw">Tweets by MinSaludCol</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+
+        <div class="mainDiagnosisGap"></div>
+    </div>
+    <div class="mainMeetUcare">
+        <h3>Conoce nuestra más de Ucare</h3>
+        <hr>
+        <div class="mainMeetUcareFeatures">
+            <div class="mainMeetUcareFeaturesGap"></div>
+            <div class="mainMeetUcareFeaturesUpLeft">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.diagnostico.svg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.sintomas.svg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.contagio.svg" alt="">    
+            </div>
+            <div class="mainMeetUcareFeaturesImage">
+                <div class="centerPhone"></div>
+            </div>
+            <div class="mainMeetUcareFeaturesDownRight">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.prevencion.svg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.estadistica.svg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.noticias.flasas.svg" alt="">
+            </div>
+            <div class="mainMeetUcareFeaturesGap"></div>
+        </div>
+    </div>
+    <div class="preFooter">
+        <div class="preFooterGap"></div>
+        <div class="preFooterMainSection">
+            <div class="resume">
+                <img src="<?php echo get_template_directory_uri() ?>/img/logo.ucare.footer.svg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>
+                <h4>Disponible en:</h4>
+                <div class="stores">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/img.google.play.svg" alt="Gooogle play">
+                <img src="<?php echo get_template_directory_uri() ?>/img/img.app.store.svg" alt="App store">
+                </div>
+            </div>
+            <div class="contact-us">
+                <p>
+                    Nosotros<br>
+                    <a>Términos y condicionens</a><br>
+                    Contacto: <a>contacto@ucare-app.com</a><br>
+                    Tel: <a>+57 301 123 4567</a><br>
+                </p>
+            </div>
+        </div>
+        <div class="preFooterGap"></div>
+    </div>
+    <footer class="footer">
+        <p>By <a href="#">@Ucare</a></p>
+    </footer>
+</body>
+</html>
